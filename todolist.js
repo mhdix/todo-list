afcaf71f24e3a05d4d37a676278339dc33ae613a
mhdix <domanloom@gmail.com>
2024-06-07 02:58:22 -0700

    idn

--- todolist.js
+++ todolist.js
@@ -1,8 +1,8 @@
 // form => sumbit => create new Todo => {id, createdAt, title:, isCOmplated}
 // const todos = [] => todos.push( ... )
 
-let todos = [];
-
+// let todos = [];
+let filterValue = "all";
 const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
 const todoList = document.querySelector(".todolist");
@@ -20,10 +20,12 @@ function addNewTodo(e) {
     isComplated: false,
   };
 
-  todos.push(newTodo);
 
+  let todos = saveTodo(newTodo);
+  // todos.push(newTodo);
   //create todos on DOM
   createTodo(todos);
+  filterTodos();
 }
 
 // create todo on DOM fun
@@ -31,13 +33,18 @@ function createTodo(todos) {
   let result = "";
   todos.forEach((todo) => {
     result += `<li class="todo" style="display: flex; align-items: center; gap: 10px; border: 1px solid black; width: fit-content; padding: 10px 20px; border-radius: 8px;">
-                    <p class="todo__title ${todo.isComplated && 'completed'}">${todo.title}</p>
+                    <p class="todo__title ${todo.isComplated && "completed"}">${
+      todo.title
+    }</p>
                     <span class="todo__createdAt">${new Date(
                       todo.createdAt
                     ).toLocaleDateString("fa-IR")}</span>
                     <button class="todo__check" data-todo-id=${
                       todo.id
-                    }><i class="far fa-check-square"></i></button>
+                    }><i class="far fa-check-square"></i></button>  
+                    <button class="todo__edit" data-todo-id=${
+                      todo.id
+                    }><i class="far fa-edit"></i></button>
                     <button class="todo__remove" data-todo-id=${
                       todo.id
                     }><i class="far fa-trash-alt"></i></button>
@@ -60,12 +67,15 @@ function createTodo(todos) {
 // filter Todos video
 const selectTodo = document.querySelector(".filter-todos");
 
-selectTodo.addEventListener("change", filterTodos);
+selectTodo.addEventListener("change", (e) => {
+  filterValue = e.target.value;
+  filterTodos();
+});
 
-function filterTodos(e) {
-  const filter = e.target.value;
-
-  switch (filter) {
+function filterTodos() {
+  // const filter = e.target.value;
+  let todos = getAllTodo()
+  switch (filterValue) {
     case "all": {
       createTodo(todos);
       break;
@@ -88,10 +98,9 @@ function filterTodos(e) {
 // removeBtn.addEventListener("click", removeTodo);
 // Delete Todo ❌video
 function removeTodo(e) {
-  const todoId = e.target.dataset.todoId;
-  const removeTodo = todos.filter((todo) => todo.id != todoId);
-  todos = removeTodo
-  createTodo(todos);
+  const todoId = Number(e.target.dataset.todoId);
+  todos = todos.filter((todo) => todo.id != todoId);
+  filterTodos();
 }
 
 // delete todo --self
@@ -104,8 +113,35 @@ function removeTodo(e) {
 
 // check todo ❌Video
 function checkTodo(e) {
+  let todos = getAllTodo()
   const todoId = Number(e.target.dataset.todoId);
   const todo = todos.find((t) => t.id === todoId);
   todo.isComplated = !todo.isComplated;
-  createTodo(todos);
+  filterTodos();
+}
+
+// function editTodo(e) {
+// }
+
+// Local Storage
+
+//show todods ()
+// document.addEventListener('DOMcontentLoaded', (e)=> {
+//   const todos = getAllTodo()
+//   createTodo(todos)
+// })
+function getAllTodo() {
+  const savedTodos = JSON.parse(localStorage.getItem("todos") || []);
+  return savedTodos;
+}
+
+function saveTodo(todo) {
+  let savedTodos = getAllTodo();
+  savedTodos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+  return savedTodos;
+}
+
+function saveAllTodos(todo) {
+  localStorage.saveTodo(todo)
 }
